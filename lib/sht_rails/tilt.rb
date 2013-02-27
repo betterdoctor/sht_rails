a98bb10169db541247ee1ca0af3b4f400952090f
zane <zane@zanedev.com>
2013-02-27 15:49:18 -0800

    testing changes

--- lib/sht_rails/tilt.rb
+++ lib/sht_rails/tilt.rb
@@ -15,17 +15,18 @@ module ShtRails
     def evaluate(scope, locals, &block)
       template_key = path_to_key scope
       <<-HandlebarsTemplate
-  (function() { 
-  #{namespace} || (#{namespace} = {});
-  #{namespace}CachedShtTemplates || (#{namespace}CachedShtTemplates = {});
-  #{namespace}CachedShtTemplates[#{template_key.inspect}] = Handlebars.compile(#{data.inspect});
-  #{namespace}[#{template_key.inspect}] = function(object) {
-    if (object == null){
-      return #{ShtRails.template_namespace}CachedShtTemplates[#{template_key.inspect}];
-    } else {
-      return #{ShtRails.template_namespace}CachedShtTemplates[#{template_key.inspect}](object);
-    }
-  };
+  (function() {
+  alert("test!")
+  ##{namespace} || (#{namespace} = {});
+  ##{namespace}CachedShtTemplates || (#{namespace}CachedShtTemplates = {});
+  ##{namespace}CachedShtTemplates[#{template_key.inspect}] = Handlebars.compile(#{data.inspect});
+  ##{namespace}[#{template_key.inspect}] = function(object) {
+  #  if (object == null){
+  #    return #{ShtRails.template_namespace}CachedShtTemplates[#{template_key.inspect}];
+  #  } else {
+  #    return #{ShtRails.template_namespace}CachedShtTemplates[#{template_key.inspect}](object);
+  #  }
+  #};
   }).call(this);
       HandlebarsTemplate
     end
