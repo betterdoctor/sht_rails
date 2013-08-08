fa5514be682451184f3da74405c3819fcc7e4b3c
zane <zane@zanedev.com>
2013-08-08 15:36:18 -0700

    fixed handlebars partial cacheing issue

--- lib/sht_rails/handlebars.rb
+++ lib/sht_rails/handlebars.rb
@@ -4,19 +4,37 @@ require "active_support"
 module ShtRails
 
   module Handlebars
+    def self.has_partial?(name)
+      if @context.present?
+        current_partials = @context.instance_variable_get(:@partials)
+        return current_partials.key?(name)
+      end
+
+      false
+    end
+
     def self.context(partials = nil)
       @context = nil unless ActionView::Resolver.caching?
+
       @context ||= begin
         context = ::Handlebars::Context.new
+
         if helpers = Rails.application.assets.find_asset(ShtRails.helper_path)
           context.runtime.eval helpers.source
         end
-        partials.each { |key, value| context.register_partial(key, value) } if partials
+
         context
       end
+
+      partials.each do |k,v|
+        @context.register_partial(k,v) unless has_partial?(k)
+      end
+
+      @context
     end
 
     def self.call(template)
+      Rails.logger.info "------------------template: #{template}"
       if template.locals.include?(ShtRails.action_view_key.to_s) || template.locals.include?(ShtRails.action_view_key.to_sym)
 <<-SHT
   hbs_context_for_sht = if defined?(partials) && partials.is_a?(Hash)
